9df121cd7146e222d09931eebecfb323e9651de3
jfmolano1587 <jose.francisco.molano@gmail.com>
2017-01-08 00:44:44 -0500

    Ecological Bin Packing solved, closes #2

--- Uva/src/ecologicalBinPacking/Main.java
+++ Uva/src/ecologicalBinPacking/Main.java
@@ -41,15 +41,49 @@ class Main
     {
         String input;
         StringTokenizer idata;
-        int [] numbers;
+        int [][] numbers;
+        String [] tags = new String[6];
+        tags[0] = "BCG";
+        tags[1] = "BGC";
+        tags[2] = "CBG";
+        tags[3] = "CGB";
+        tags[4] = "GBC";
+        tags[5] = "GCB";
 
         while ((input = Main.ReadLn (255)) != null)
         {
           idata = new StringTokenizer (input);
-          numbers = new int[9];
+          numbers = new int[3][3];
           for(int i=0;i<9;i++){
-        	  numbers[i] = Integer.parseInt(idata.nextToken());
+        	  numbers[i/3][i%3] = Integer.parseInt(idata.nextToken());
           }
+          int [] sumas = new int[6];
+          int b_1 = numbers[1][0] + numbers[2][0];
+          int b_2 = numbers[0][0] + numbers[2][0];
+          int b_3 = numbers[0][0] + numbers[1][0];
+          int g_1 = numbers[1][1] + numbers[2][1];
+          int g_2 = numbers[0][1] + numbers[2][1];
+          int g_3 = numbers[0][1] + numbers[1][1];
+          int c_1 = numbers[1][2] + numbers[2][2];
+          int c_2 = numbers[0][2] + numbers[2][2];
+          int c_3 = numbers[0][2] + numbers[1][2];
+          sumas[0] = b_1 + c_2 + g_3;
+          sumas[1] = b_1 + g_2 + c_3;
+          sumas[2] = c_1 + b_2 + g_3;
+          sumas[3] = c_1 + g_2 + b_3;
+          sumas[4] = g_1 + b_2 + c_3;
+          sumas[5] = g_1 + c_2 + b_3;
+          
+          int min = Integer.MAX_VALUE;
+          int tag_index = 0;
+          
+          for(int i=0;i<6;i++){
+        	  if(min > sumas[i]){
+        		  min = sumas[i];
+        		  tag_index = i;
+        	  }
+          }
+          System.out.println(tags[tag_index] +" "+ min);
         }
     }
 }
