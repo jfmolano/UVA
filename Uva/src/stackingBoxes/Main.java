43620c66b424f0d7a266dff9ecfe8509a96a7143
jfmolano1587 <jose.francisco.molano@gmail.com>
2017-01-10 00:53:45 -0500

    Box ordered by dimentions and sum of dimentions

--- Uva/src/stackingBoxes/Main.java
+++ Uva/src/stackingBoxes/Main.java
@@ -40,20 +40,64 @@ class Main
     {
         String input;
         StringTokenizer idata;
-        int a, b, min, max, num, n, cycle, cyclemax;
 
         while ((input = Main.ReadLn (255)) != null)
         {
           idata = new StringTokenizer (input);
-          a = Integer.parseInt (idata.nextToken());
-          b = Integer.parseInt (idata.nextToken());
-          if (a < b) { min=a; max=b; } else { min=b; max=a; }
-          for (cyclemax=-1, num=min; num<=max; num++) {
-            for (n=num, cycle=1; n != 1; cycle++) if ((n % 2) != 0) n=3*n+1; else n >>= 1;
-            if (cycle > cyclemax) cyclemax=cycle;
-            }
-          System.out.println (a + " " + b + " " + cyclemax);
+          int num_boxes = Integer.parseInt (idata.nextToken());
+          int num_dimentions = Integer.parseInt (idata.nextToken());
+          List<Box> boxes_order = new ArrayList<Box>();
+          for (int i = 0;i<num_boxes;i++){
+        	  int ordered_box [] = new int[num_dimentions];
+        	  StringTokenizer box_numbers = new StringTokenizer ( Main.ReadLn (255) );
+        	  int dim_sum = 0;
+        	  for (int j=0;j<num_dimentions;j++){
+        		  int dim = Integer.parseInt (box_numbers.nextToken());
+        		  ordered_box[j] = dim;
+        		  dim_sum += dim;
+        	  }
+        	  Arrays.sort(ordered_box);
+        	  Box cu_box = new Box(ordered_box,dim_sum,i+1);
+        	  boxes_order.add(cu_box);
+          }
+          Collections.sort(boxes_order, new BoxComparator());
+          for(int k=0;k<boxes_order.size();k++){
+        	  System.out.print(boxes_order.get(k).getId()+" - ");
+              for(int l=0;l<boxes_order.get(k).getDim().length;l++){
+            	  System.out.print(boxes_order.get(k).getDim()[l]+" ");
+              }
+        	  System.out.println("");
+          }
+        }
+    }
+    
+    public class Box {
+    	private int[] dimentions;
+    	private int sum;
+    	private int box_id;
+        public Box(int[] dimentions,int sum,int box_id){
+        	this.dimentions = dimentions;
+        	this.sum = sum;
+        	this.box_id = box_id;
+        }
+        public int[] getDim(){
+        	return dimentions;
+        }
+        public int getSum(){
+        	return sum;
+        }
+        public int getId(){
+        	return box_id;
+        }
+    }
+    
+    public class BoxComparator implements Comparator<Box> {
+        @Override
+        public int compare(Box o1, Box o2) {
+            return o1.getSum() - o2.getSum();
         }
     }
 }
+
+
 
